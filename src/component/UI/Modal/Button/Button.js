833e6b345a0f934f53f1f3315693c92f2ce515a5
Manoj Sahu <mnojsha413@gmail.com>
2021-06-15 21:43:52 +0530

    fromValidation

--- src/component/UI/Modal/Button/Button.js
+++ src/component/UI/Modal/Button/Button.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Button = (props) => {
     return (
         <button 
+            disabled={props.disabled}
             className={'Button', [props.btnType].join(' ')}
             onClick={props.clicked}>{props.children}</button>
     )
